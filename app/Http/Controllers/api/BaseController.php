71ab5447edd8af5e9542959ee31b578a0c71eff7
Rina Apriana <rinaaprianarinaapriana404@gmail.com>
2026-06-07 21:41:12 +0800

    Menyelesaikan CRUD Item API

--- app/Http/Controllers/api/BaseController.php
+++ app/Http/Controllers/api/BaseController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 
-class BaseController extends Controller {
-
-    protected function success($data = null, $message = null, $code = 200) {
+class BaseController extends Controller
+{
+    protected function success($data = null, $message = null, $code = 200)
+    {
         return response()->json([
             'success' => true,
-            'data'    => $data,
+            'data' => $data,
             'message' => $message,
         ], $code);
     }
 
-    protected function error($message = null, $code = 400) {
+    protected function error($message = null, $code = 400)
+    {
         return response()->json([
             'success' => false,
             'message' => $message,
---
+++ app/services/ItemService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Item;
+
+class ItemService
+{
+    public function all()
+    {
+        return Item::all();
+    }
+
+    public function find($id)
+    {
+        return Item::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Item::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $item = Item::findOrFail($id);
+
+        $item->update($data);
+
+        return $item;
+    }
+
+    public function delete($id)
+    {
+        $item = Item::findOrFail($id);
+
+        return $item->delete();
+    }
+}
